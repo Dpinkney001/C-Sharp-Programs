b0db9c2f7738dacbc8f86cf99821deab9dd63b42
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2019-02-26 12:10:30 -0500

    Add files via upload

---
+++ SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bank_Account
+{
+    class SavingsAccount
+    {
+        Double annualInterestRate = 0;
+        private Double savingsBalance;
+        private Double newBalance = 0;
+        Double interestRate = 0;
+
+        public SavingsAccount(Double savingsBalance)
+        {
+            Double CalculateMonthlyInterest()
+            {
+                newBalance = (savingsBalance * annualInterestRate) / 12;
+
+                return newBalance;
+            }// end CalculateMonthlyInterestRate
+
+            Double ModifyInterestRate(Double interestRate)
+            {
+                annualInterestRate = interestRate;
+
+                return annualInterestRate;
+            }// end ModifyInterestRate
+        }
+    }
+}
+    
---
+++ SavingsAccountTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static Bank_Account.SavingsAccount;
+
+
+namespace Bank_Account
+{
+    class SavingsAccountTest
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount saver1 = new SavingsAccount(2000.00);
+            SavingsAccount saver2 = new SavingsAccount(3000.00);
+            Double newInterestRate;
+            Double saver1Balances;
+            Double saver2Balances;
+
+            newInterestRate = ModifyInterestRate(0.04);
+            saver1Balances = saver1.CalculateMonthlyInterest();
+            saver2Balances = saver2.CalculateMonthlyInterest();
+            Console.WriteLine("Savings account saver1 balance is now: ", saver1Balance);
+
+            saver2Balance = saver2.ModifyInterestRate(0.05);
+
+        }
+
+       
+    }
+}
